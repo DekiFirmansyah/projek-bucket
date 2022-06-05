1c307d407ab83dde88d3fbb7c855f1c66316decf
DekiFirmansyah <dekifirmansyah99@gmail.com>
2022-06-06 01:12:40 +0700

    Database Tabel

---
+++ database/migrations/2022_06_05_174126_create_transaksi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jumlah');
+            $table->integer('jumlah_tambah');
+            $table->integer('tota_harga');
+            $table->string('tgl_transaksi');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
